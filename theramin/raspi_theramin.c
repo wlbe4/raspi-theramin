9d8cc17d8923fc060e109f10323dd34f28b8c04f
Lior Weintraub <LiorW@pliops.com>
2025-03-28 13:38:37 +0300

    1. Add MIDI play note functions 2. Convert the distance read from ToF1020 into MIDI notes

--- theramin/raspi_theramin.c
+++ theramin/raspi_theramin.c
@@ -4,14 +4,68 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <alsa/asoundlib.h>
+#include "pitch_to_note.h"
 
 #define TOF1020_DEV "/dev/tof1020"
-
 // Yamaha Clavinova USB ID 0499:1613
 // amidi -l
 // Dir Device    Name
 // IO  hw:0,0,0  Clavinova MIDI 1
 #define MIDI_PORT_NAME "hw:0,0,0"
+typedef struct {
+    char cmd[3];
+}midiEventPacket_t;
+
+#define MAX_TOF_VAL     0x180
+#define MOUT_CH         2 // MIDI 0-15 is mapped to device midi channel 1-16
+#define NORM_VELOCITY   64
+#define FROM_NOTE   pitchC3
+#define TO_NOTE     pitchC4
+#define NOTE_RANGE (TO_NOTE-FROM_NOTE)
+
+// The channel range is 0-15
+// The control range is 0-119.
+// The value range is 0-127.
+int controlChange(snd_rawmidi_t * midiout, char channel, char control, char value) {
+    midiEventPacket_t event = {0xB0 | channel, control, value};
+    return snd_rawmidi_write(midiout, &event, 3);
+}
+
+// MIDI Stuff
+// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// Second parameter is note-on/note-off, combined with the channel.
+// Channel can be anything between 0-15. Typically reported to the user as 1-16.
+// Third parameter is the note number (48 = middle C).
+// Fourth parameter is the velocity (64 = normal, 127 = fastest).
+
+int noteOn(snd_rawmidi_t * midiout, char channel, char pitch, char velocity) {
+    midiEventPacket_t noteOn = {0x90 | channel, pitch, velocity};
+    return snd_rawmidi_write(midiout, &noteOn, 3);
+}
+
+int noteOff(snd_rawmidi_t * midiout, char channel, char pitch, char velocity) {
+    midiEventPacket_t noteOff = {0x80 | channel, pitch, velocity};
+    return snd_rawmidi_write(midiout, &noteOff, 3);
+}
+
+void play_note(snd_rawmidi_t * midiout, char note)
+{
+    int status;
+    // char note = (char)notePitch[idx];
+    if ( (status = noteOn(midiout, MOUT_CH, note, NORM_VELOCITY)) < 0 ) {
+        fprintf(stderr, "Failed to send MIDI noteOn command\n");
+    }
+    usleep(100*1000);
+    if ( (status = noteOff(midiout, MOUT_CH, note, NORM_VELOCITY)) < 0 ) {
+        fprintf(stderr, "Failed to send MIDI noteOn command\n");
+    }
+}
+
+void play_val(snd_rawmidi_t * midiout, uint16_t val, uint16_t max_val)
+{
+    char note = FROM_NOTE + (NOTE_RANGE * val / max_val);
+    play_note(midiout, note);
+}
 
 int main(int argc, char const *argv[])
 {
@@ -21,29 +75,68 @@ int main(int argc, char const *argv[])
     int mode = SND_RAWMIDI_SYNC;
     snd_rawmidi_t * midiout = NULL;
     const char * portname = MIDI_PORT_NAME;
-    uint16_t val;
-
+    uint16_t val = 1;
+    uint16_t prev_val = 1;
     if (tof1020file < 0) {
         fprintf(stderr, "Failed to open %s\n",TOF1020_DEV);
         return -1;
     }
-
     /** MIDI SETUP */
     if ((status = snd_rawmidi_open(NULL, &midiout, portname, mode)) < 0) {
         fprintf(stderr, "Failed to open MIDI: %s\n",snd_strerror(status));
         return -1;
     }
-    printf("MIDI port %s was open successfuly\n", portname);
 
-    if (pread(tof1020file, &val,sizeof(val),0) == sizeof(val)) {
-        printf("Read from ToF value: 0x%x\n",val);
-    } else {
-        fprintf(stderr, "Failed to read 2 bytes from %s\n",TOF1020_DEV);
+    while (val) {
+        if (pread(tof1020file, &val,sizeof(val),0) == sizeof(val)) {
+            if (abs(prev_val-val) > 50) {
+                printf("Read from ToF value: 0x%x\n",val);
+                prev_val = val;
+                if (val < MAX_TOF_VAL) {
+                    play_val(midiout, val, MAX_TOF_VAL);
+                }
+            }
+        } else {
+            fprintf(stderr, "Failed to read 2 bytes from %s\n",TOF1020_DEV);
+        }
+        usleep(100*1000); // Sample distance with 10Hz
     }
+#if 0
+    play_note(midiout, pitchG3); usleep(200*1000);
+    play_note(midiout, pitchE3); usleep(200*1000);
+    play_note(midiout, pitchE3); usleep(200*1000);
+    usleep(300*1000);
+    play_note(midiout, pitchF3); usleep(200*1000);
+    play_note(midiout, pitchD3); usleep(200*1000);
+    play_note(midiout, pitchD3); usleep(200*1000);
+    usleep(300*1000);
+    play_note(midiout, pitchC3); usleep(200*1000);
+    play_note(midiout, pitchD3); usleep(200*1000);
+    play_note(midiout, pitchE3); usleep(200*1000);
+    play_note(midiout, pitchF3); usleep(200*1000);
+    play_note(midiout, pitchG3); usleep(200*1000);
+    play_note(midiout, pitchG3); usleep(200*1000);
+    play_note(midiout, pitchG3); usleep(200*1000);
+    usleep(300*1000);
 
+    play_note(midiout, pitchG3); usleep(200*1000);
+    play_note(midiout, pitchE3); usleep(200*1000);
+    play_note(midiout, pitchE3); usleep(200*1000);
+    usleep(300*1000);
+    play_note(midiout, pitchF3); usleep(200*1000);
+    play_note(midiout, pitchD3); usleep(200*1000);
+    play_note(midiout, pitchD3); usleep(200*1000);
+    usleep(300*1000);
+    play_note(midiout, pitchC3); usleep(200*1000);
+    play_note(midiout, pitchE3); usleep(200*1000);
+    play_note(midiout, pitchG3); usleep(200*1000);
+    play_note(midiout, pitchG3); usleep(200*1000);
+    play_note(midiout, pitchC3); usleep(200*1000);
+    usleep(300*1000);
+#endif
     // Close MIDI device
     if ((status = snd_rawmidi_close(midiout)) < 0) {
         fprintf(stderr, "Failed to close MIDI: %s\n",snd_strerror(status));
         return -1;
-    }
+    }    
 }
